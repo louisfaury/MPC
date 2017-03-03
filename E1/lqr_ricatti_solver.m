b1f439dad8492f1e8451378096f082709cf65572
gregallois <gregoire.g-m@hotmail.fr>
2017-03-03 11:40:20 +0100

    Matlab Ex3 + minor changes in report

--- E1/lqr_ricatti_solver.m
+++ E1/lqr_ricatti_solver.m
@@ -1,11 +1,13 @@
-function K_on_horizon = lqr_ricatti_solver(A,B,Q,R,N)
+function [K_on_horizon, H0] = lqr_ricatti_solver(A,B,Q,R,N)
 % @brief DP solving of the MPC
 % @param   x_{k+1} = Ax_k + Bu_k
 %          Q,R are the weighting matrixes of the MPC problem
 %          N is the horizon
-% @returns The sequence of linear feedback laws for 1,..,N
+% @returns K_on_horizon, the sequence of linear feedback laws for 1,..,N
+%          H0, such that cost is x_k'H0x_k           
 
 K_on_horizon = zeros(N,2);
+H_on_horizon = zeros(N,2);
 
 H = Q;
 for i=N:-1:1
@@ -13,5 +15,7 @@ for i=N:-1:1
     H = Q+K'*R*K +(A+B*K)'*H*(A+B*K);
     K_on_horizon(i,:) = K;
 end
+
+H0=H;
 
 end
